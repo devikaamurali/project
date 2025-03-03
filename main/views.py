755e174565cff8eaf39b613950c18540d41535a8
devikaamurali <devikamurali940@gmail.com>
2025-03-03 16:17:22 +0530

    03 03 news ,about

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import requests
 
 
 # Create your views here.
@@ -21,8 +22,19 @@ def verify(request):
     return render(request, 'linkverify.html')
 
 def cyber(request):
-    return render(request, 'cyber.html')
+    api_url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=ffbf00a3d838482bb9bc980e0f5b61a7'
+    news_articles = []
 
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()  # Raise an error for bad responses
+        data = response.json()
+        news_articles = data.get('articles', [])
+    except requests.exceptions.RequestException as e:
+        print('Error fetching news:', e)
+        # You can handle the error as needed, e.g., log it or set a message for the template
+
+    return render(request, 'cyber.html', {'news_articles': news_articles})
 def short(request):
     return render(request, 'short.html')
 
